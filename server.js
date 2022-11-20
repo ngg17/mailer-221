68580bf1221ab98cc055ed45e596f57a4ac8f1b2
Noyonika Gupta <71960903+ngg17@users.noreply.github.com>
2022-11-20 09:52:37 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.use(express.json())
+
+var nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    port:465,
+    host:"smtp.gmail.com",
+    auth:{
+        user:'noyonikagg170608@gmail.com',
+        pass:'hstuzoviktpleopx',
+    },
+    secure:true,
+})
+
+
+server.listen(process.env.PORT || 3030);
